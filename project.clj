dfb454f3abba25c2e9782ab8374bfe57d2df260f
Jon Eland <jonathan.eland@gmail.com>
2013-02-15 17:39:20 +0000

    Tidying up project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject myproject "0.1.0-SNAPSHOT"
-  :description "Clojure kata"
-  :url "http://example.com/FIXME"
+  :description "Clojure kata's"
+  :url "https://github.com/joneland/clojure-katas"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
