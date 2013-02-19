870948ef36bef18fdbc5dad7641e598c0ccb0406
Jon Eland <jonathan.eland@gmail.com>
2013-02-19 13:54:00 +0000

    First attempt at prime factors - certain I can clean up the implementation further

--- src/primefactors/core.clj
+++ src/primefactors/core.clj
@@ -1,2 +1,15 @@
 (ns primefactors.core)
 
+(defn smallest-prime [x current]
+  (if (zero? (mod x current))
+    current
+    (smallest-prime x (inc current))))
+
+(defn prime-factors [x so-far]
+  (if (= x 1)
+    (reverse so-far)
+    (prime-factors (/ x (smallest-prime x 2))
+                   (conj so-far (smallest-prime x 2)))))
+
+(defn generate [x]
+  (prime-factors x '()))
--- test/primefactors/core_test.clj
+++ test/primefactors/core_test.clj
@@ -1,2 +1,16 @@
-(ns primefactors.core-test)
+(ns primefactors.core-test
+  (:use midje.sweet
+        primefactors.core))
 
+(fact "smallest prime is the lowest number divisible by the given integer"
+      (smallest-prime 2 2) => 2
+      (smallest-prime 3 2) => 3
+      (smallest-prime 10 2) => 2)
+
+(fact "prime factors is a list of prime factors that make up the given integer"
+      (prime-factors 8 []) => '(2 2 2))
+
+(fact
+  (generate 2) => '(2)
+  (generate 6) => '(2 3)
+  (generate 10) => '(2 5))
