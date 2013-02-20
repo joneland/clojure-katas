62238f925153bc3abe7de35f6fbf4e3a53988341
Jon Eland <jonathan.eland@gmail.com>
2013-02-20 12:59:42 +0000

    Removed duplication of calling smallest-prime by using let

--- src/primefactors/core.clj
+++ src/primefactors/core.clj
@@ -8,8 +8,9 @@
 (defn prime-factors [x so-far]
   (if (= x 1)
     (reverse so-far)
-    (prime-factors (/ x (smallest-prime x 2))
-                   (conj so-far (smallest-prime x 2)))))
+    (let [next-prime (smallest-prime x 2)]
+      (prime-factors (/ x next-prime)
+                     (conj so-far next-prime)))))
 
 (defn generate [x]
   (prime-factors x '()))
