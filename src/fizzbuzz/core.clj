f83906daff349262f49890ebc41b14b732b39777
Jon Eland <jonathan.eland@gmail.com>
2013-02-21 13:16:12 +0000

    Added solution to FizzBuzz kata

---
+++ src/fizzbuzz/core.clj
@@ -0,0 +1,23 @@
+(ns fizzbuzz.core)
+
+(def make-divider
+  (fn [divider]
+    (fn [x]
+      (zero? (mod x divider)))))
+
+(def divisible-by-3 (make-divider 3))
+
+(def divisible-by-5 (make-divider 5))
+
+(defn print-output [x]
+  (if (and (divisible-by-3 x)
+           (divisible-by-5 x))
+    "FizzBuzz"
+    (if (divisible-by-3 x)
+      "Fizz"
+      (if (divisible-by-5 x)
+        "Buzz"
+        x))))
+
+(defn print-range [upper-limit]
+  (map print-output (range 1 (inc upper-limit))))
---
+++ test/fizzbuzz/core_test.clj
@@ -0,0 +1,27 @@
+(ns fizzbuzz.core-test
+  (:use midje.sweet
+        fizzbuzz.core))
+
+(fact "returns true if number is divisible by 3"
+      (divisible-by-3 5) => false
+      (divisible-by-3 12) => true)
+
+(fact "returns true if number is divisible by 5"
+      (divisible-by-5 7) => false
+      (divisible-by-5 20) => true)
+
+(fact "Fizz is printed instead of numbers divisible by 3"
+      (print-output 9) => "Fizz"
+      (print-output 8) => 8)
+
+(fact "Buzz is printed instead of numbers divisible by 5"
+      (print-output 10) => "Buzz"
+      (print-output 8) => 8)
+      
+(fact "FizzBuzz is printed instead of numbers divisible by 3 and 5"
+      (print-output 15) => "FizzBuzz"
+      (print-output 8) => 8)
+
+(fact "prints 1 to specified upper limit whilst substituting numbers divisible by 3 and 5"
+  (print-range 15)
+  => [1 2 "Fizz" 4 "Buzz" "Fizz" 7 8 "Fizz" "Buzz" 11 "Fizz" 13 14 "FizzBuzz"])
